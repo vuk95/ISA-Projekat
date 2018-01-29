572db2b9e400c4a0bc2374aa72c30a02f0382ad0
vuknovakovic <vuk95novakovic@gmail.com>
2018-01-29 19:23:02 +0100

    Izabrane Maven zavisnosti: Web, JPA, MySQL, Actuator

---
+++ Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/CinemaApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn.informatika.Cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApplication.class, args);
+	}
+}
