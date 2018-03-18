48958381ae21914a21a7e5284e28e5cca877a48c
dimmih <dimmihzr@gmail.com>
2018-03-18 13:00:48 +0100

    Krenuo sam da radim login preko spring security
    Treba da se vidi kako ce se odraditi UserDetailsService

---
+++ Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/LoginController.java
@@ -0,0 +1,18 @@
+package rs.ac.uns.ftn.informatika.Cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView model = new ModelAndView("login");
+		
+		return model;	
+	}
+	
+}
