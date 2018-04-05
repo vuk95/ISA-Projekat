a18879b232664ed4ad2be28c27b74f9b2a85dae9
Milovic <Milovic@DESKTOP-BHBIRQC>
2018-04-05 18:04:55 +0200

    Kompletiran update za pozorista i bioskope.

--- Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/PozoristaIBioskopiController.java
+++ Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/PozoristaIBioskopiController.java
@@ -3,10 +3,12 @@ package rs.ac.uns.ftn.informatika.Cinema.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import rs.ac.uns.ftn.informatika.Cinema.model.CinemaTheatre;
 import rs.ac.uns.ftn.informatika.Cinema.service.CinemaTheatreService;
 
 @Controller
@@ -50,6 +52,34 @@ public class PozoristaIBioskopiController {
 		map.put("bioskop",service.findOne(id));
 		
 		return "izmeniBioskop";
+	}
+	
+	@RequestMapping(value = "/updateCinema" , method = RequestMethod.POST)
+	public String editCinema(@ModelAttribute("bioskop") CinemaTheatre bioskop,ModelMap map) {
+		
+		CinemaTheatre currentCinema = service.findOne(bioskop.getId());
+		
+		currentCinema.setName(bioskop.getName());
+		currentCinema.setAddress(bioskop.getAddress());
+		currentCinema.setDescription(bioskop.getDescription());
+		
+		service.save(currentCinema);
+		
+		return "redirect:../cinematheatre/getCinema";
+	}
+	
+	@RequestMapping(value = "/updateTheatre" , method = RequestMethod.POST)
+	public String editTheatre(@ModelAttribute("pozoriste") CinemaTheatre pozoriste ,ModelMap map) {
+		
+		CinemaTheatre currentTheatre = service.findOne(pozoriste.getId());
+		
+		currentTheatre.setName(pozoriste.getName());
+		currentTheatre.setAddress(pozoriste.getAddress());
+		currentTheatre.setDescription(pozoriste.getDescription());
+
+	    service.save(currentTheatre);
+		
+	    return "redirect:../cinematheatre/getTheatre";
 	}
 	
 }
