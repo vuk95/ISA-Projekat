42d46b295818d86cecc737ee53e3beba744688a6
Milovic <Milovic@DESKTOP-BHBIRQC>
2018-04-15 22:15:50 +0200

    Dodate sitnice.

--- Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/ProjectionImageController.java
+++ Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/ProjectionImageController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import rs.ac.uns.ftn.informatika.Cinema.model.Projections;
+import rs.ac.uns.ftn.informatika.Cinema.service.CinemaTheatreService;
 import rs.ac.uns.ftn.informatika.Cinema.service.ProjectionImageService;
 import rs.ac.uns.ftn.informatika.Cinema.service.ProjectionsService;
 
@@ -27,8 +28,8 @@ public class ProjectionImageController {
 	@Autowired
 	private ProjectionsService  pservice;
 	
-	//@Autowired
-	//private  CinemaTheatreService service;
+	@Autowired
+	private  CinemaTheatreService service;
 	
 	@Autowired
 	private ProjectionImageService imgservice;
@@ -130,5 +131,55 @@ public class ProjectionImageController {
 		    }
 	 
 	 
+		/* @GetMapping("/getPredstave/{id}/addPredstaveImage")
+		    public String showUploadAddTheatreForm(@PathVariable Long id, ModelMap map){
+
+		        map.put("pozoriste",service.findOne(id));
+		        
+		        return "dodajimagepredstave";
+		    }
+			
+			 @PostMapping("/getPredstave/{id}/addPredstaveImage")
+			 public String handleAddTheatreImagePost(@PathVariable Long id, @RequestParam("imagefile") MultipartFile file,ModelMap map){
+
+				 
+				 	map.put("pozoriste",service.findOne(id));
+				 	imgservice.saveImageFile(id,file);
+
+				 	//String referer = request.getHeader("Referer");
+					
+					//return "redirect:"+ referer;
+				 	
+			        return "redirect:/cinematheatre/getPredstave/" + id;
+			    }
+			
+			
+			 @GetMapping("/getPredstave/{id}/pozoristeimage")
+			 public void renderAddTheatreImage(@PathVariable Long id, HttpServletResponse response) throws IOException {
+			    	
+			    	Projections projekcija = pservice.findOne(id);
+			        
+			        if(projekcija == null){
+			            throw new RuntimeException("there is no rekvizit with this id : " + id);
+			        }
+
+			        //baca null exception na ovoj liniji koda zato sto u bazi u ovom trenutku nema slike
+			        //pa ne moze da vrati length
+			        byte[] byteArray = new byte[projekcija.getPicture().length];
+			        int i= 0;
+			      
+			        for(Byte wrappedByte : projekcija.getPicture() ){
+
+			            byteArray[i++] = wrappedByte;
+			        }
+			        
+			        response.setContentType("image/jpeg");
+			        InputStream is = new ByteArrayInputStream(byteArray);
+			        IOUtils.copy(is,response.getOutputStream());
+
+
+			    }
+		 
+		 */
 	
 }
