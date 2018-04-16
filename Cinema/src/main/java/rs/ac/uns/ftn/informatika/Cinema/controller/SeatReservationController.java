bcf1ba92b2e3f54524d2b943c56e5f0daf54b50a
dimmih <dimmih@users.noreply.github.com>
2018-04-16 15:05:16 +0200

    Treba jos ubaciti u korisnikove rezervacije, posecene bioskope/pozorista i otkazivanje rezervacija

--- Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/SeatReservationController.java
+++ Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/controller/SeatReservationController.java
@@ -1,6 +1,12 @@
 package rs.ac.uns.ftn.informatika.Cinema.controller;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import rs.ac.uns.ftn.informatika.Cinema.model.Projections;
+import rs.ac.uns.ftn.informatika.Cinema.model.Reservation;
+import rs.ac.uns.ftn.informatika.Cinema.model.users.RegularUser;
 import rs.ac.uns.ftn.informatika.Cinema.service.CinemaTheatreService;
 import rs.ac.uns.ftn.informatika.Cinema.service.ProjectionsService;
+import rs.ac.uns.ftn.informatika.Cinema.service.RegularUserService;
 
 @Controller
 public class SeatReservationController {
@@ -20,33 +29,43 @@ public class SeatReservationController {
 	@Autowired
 	private ProjectionsService projectionsService;
 	
+	@Autowired
+	private RegularUserService regularUserService;
+	
+	@PreAuthorize("hasAuthority('REGULAR')")
 	@RequestMapping(value = "/cinematheatre/getPredstave/{ctId}/{pId}", method = RequestMethod.GET)
-	public ModelAndView showPerformance(@PathVariable("ctId") Long ctId, @PathVariable("pId") Long pId) {
+	public ModelAndView showPerformance(@PathVariable("ctId") Long ctId, @PathVariable("pId") Long pId, Principal principal) {
 		ModelAndView modelAndView = new ModelAndView();
 		Projections projection = cinemaTheatreService.findMyProjectionById(ctId, pId);
+		RegularUser user = regularUserService.findByEmail(principal.getName());
 		
 		if(projection == null) {
 			modelAndView.addObject("error", "Doslo je do greske!");
 		}
 		
-		modelAndView.addObject("seatConfig", projectionsService.getSeatConfiguration(pId));
+		modelAndView.addObject("user", user);
+		modelAndView.addObject("seatConfig", projectionsService.getSeatConfiguration(projection));
+		modelAndView.addObject("reservedSeats", projectionsService.getReservedSeats(projection));
 		modelAndView.addObject("projection", projection);
 		modelAndView.setViewName("projection");
 		
 		return modelAndView;
 	}
 	
+	@PreAuthorize("hasAuthority('REGULAR')")
 	@RequestMapping(value = "/cinematheatre/getProjekcije/{ctId}/{pId}", method = RequestMethod.GET)
-	public ModelAndView showProjection(@PathVariable("ctId") Long ctId, @PathVariable("pId") Long pId) {
+	public ModelAndView showProjection(@PathVariable("ctId") Long ctId, @PathVariable("pId") Long pId, Principal principal) {
 		ModelAndView modelAndView = new ModelAndView();
-		
 		Projections projection = cinemaTheatreService.findMyProjectionById(ctId, pId);
+		RegularUser user = regularUserService.findByEmail(principal.getName());
 		
 		if(projection == null) {
 			modelAndView.addObject("error", "Doslo je do greske!");
 		}
 		
-		modelAndView.addObject("seatConfig", projectionsService.getSeatConfiguration(pId));
+		modelAndView.addObject("user", user);
+		modelAndView.addObject("seatConfig", projectionsService.getSeatConfiguration(projection));
+		modelAndView.addObject("reservedSeats", projectionsService.getReservedSeats(projection));
 		modelAndView.addObject("projection", projection);
 		modelAndView.setViewName("projection");
 		
---
+++ Cinema/src/main/java/rs/ac/uns/ftn/informatika/Cinema/repository/ReservationRepository.java
@@ -0,0 +1,11 @@
+package rs.ac.uns.ftn.informatika.Cinema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import rs.ac.uns.ftn.informatika.Cinema.model.Reservation;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
